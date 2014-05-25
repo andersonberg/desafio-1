f717421fd7827ef093f1faf44248cd4d4564af73
Anderson Berg <andersonberg@gmail.com>
2014-05-25 00:25:55 -0300

    exibe as estatísticas de cada repositório e a lista de commits

--- github_script.js
+++ github_script.js
@@ -1,10 +1,20 @@
 'use strict';
+
+var repo_commits = [];
+
+//função que é chamada ao clicar no nome de um repositório
+function stats(){
+    github.repos.filter(github.findProject, this);
+};
+
+//Classe Commit representa cada commit em um repositório
 var Commit = function(user_login, commit_message, commit_date){
     this.user_login = user_login;
     this.commit_message = commit_message;
     this.commit_date = commit_date;
 };
 
+//Classe Projeto representa um repositório
 var Projeto = function(name, stars, forks){
     this.name = name;
     this.stars = stars;
@@ -12,8 +22,9 @@ var Projeto = function(name, stars, forks){
     this.commits = [];
 };
 
+//Obtém todos os commits de um repositório
 Projeto.prototype.getCommits = function(){
-    var url = 'https://api.github.com/repos/globocom/' + this.name + '/commits';
+    var url = 'https://api.github.com/repos/globocom/' + this.name + '/commits?login=andersonberg&authToken=f94236ae56d6c2325123261daf99c439e8d2cd66';
 
     var request = new XMLHttpRequest();
     request.onload = setCommits;
@@ -21,6 +32,7 @@ Projeto.prototype.getCommits = function(){
     request.send();
 };
 
+//Cria objetos Commit e popula a lista de commits de um repositório
 function setCommits(){
     var responseObj = JSON.parse(this.responseText);
     var commits_list = [];
@@ -31,16 +43,33 @@ function setCommits(){
 
         var commit = new Commit(user_login, commit_message, commit_date);
         commits_list.push(commit);
+        // repo_commits.push(commit);
     }
+    var list_commits = document.getElementById("list_commits");
+    list_commits.innerHTML = "";
 
-    return commits_list;
+    for(var comm in commits_list){
+        list_commits.innerHTML = list_commits.innerHTML + '<li>' + commits_list[comm].user_login + ' ' + commits_list[comm].commit_message + '</li>';
+    }
 };
 
+//Classe Github representa um objeto que centraliza as informações de todos os repositórios
 var Github = function(){
     this.public_repos = 0;
     this.count_pages = 0;
     this.repos_names = [];
     this.repos = [];
+};
+
+//procura pelo nome do repositório dentro da lista e exibe suas estatísticas
+Github.prototype.findProject = function(element){
+    var stars = document.getElementById("stars");
+    var forks = document.getElementById("forks");
+    if(element.name == this.text){
+        stars.innerHTML = "stars: " + element.stars;
+        forks.innerHTML = "forks: " + element.forks;
+        element.getCommits();
+    }
 };
  
 Github.prototype.printStarsCount = function(){
@@ -62,10 +91,10 @@ Github.prototype.getReposNames = function(){
     github.repos.sort(function(a,b) {return b.stars - a.stars});
 
     var ul = document.getElementById("repos_list");
-    var items = [];
+    //var items = [];
     $.each(github.repos, function(i){
         //items.push('<a href="#"><li>' + github.repos[i].name + '</li></a>');
-        ul.innerHTML = ul.innerHTML + '<li class="sel"> <a href="#">' + github.repos[i].name + '</a></li>'; //+ ' : stars: ' + github.repos[i].stars + ' forks: ' + github.repos[i].forks + '</li>'
+        ul.innerHTML = ul.innerHTML + '<li class="sel"> <a onclick="stats.call(this);return false;">' + github.repos[i].name + '</a></li>';
     });
 };
 
@@ -90,5 +119,5 @@ Github.prototype.repoCount = function(){
 var request = new XMLHttpRequest();
 var github = new Github();
 request.onload = github.repoCount;
-request.open('get', 'https://api.github.com/users/globocom', true);
+request.open('get', 'https://api.github.com/users/globocom?login=andersonberg&authToken=f94236ae56d6c2325123261daf99c439e8d2cd66', true);
 request.send();
